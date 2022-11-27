e4232c6c121df429670ca85a11c37c761827d80a
sny430 <shinya430.g@gmail.com>
2022-11-27 12:51:36 +0900

    :fire: remove dev page link

--- client/src/pages/rooms.tsx
+++ client/src/pages/rooms.tsx
@@ -20,13 +20,9 @@ export default function Rooms() {
                         <Link href="/">
                             <p>TopPage</p>
                         </Link>
-                        <br></br>
+                        <br/>
                         <Link href="/playing">
                             <p>Start</p>
-                        </Link>
-                        <br></br>
-                        <Link href="/gamepage">
-                            <p>gamepage</p>
                         </Link>
                         <div className="bird"></div>
                         <div className="bird -type_2"></div>
